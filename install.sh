231b4c39cc7ab5387b0c5ec8a7316a337da01030
SVNice <22481082+SVNice@users.noreply.github.com>
2022-01-27 18:24:50 +0000

    breif layout of parse for arguments

--- install.sh
+++ install.sh
@@ -5,8 +5,27 @@ search_for_local_repos=true
 install_tox=true
 install_hooks=["all"] # Just passing all will install all hooks in ./hooks/
 # Get all arguments and process them
+args=("$@")
+index=0
+# If argument is not empty
+while [[ -n "${args[$index]}" ]]; do
+   current_arg="${args[$index]}"
+   case "$current_arg" in
+           "--test" | "-t")
+                   index=$index+1
+                   echo "test arg is ${args[$index]}"
+                   ;;
+           *)
+                   echo "Unkown argument $current_arg"
+                   exit 1
+   esac
+   index=$index+1
+done
+
 
 # Get all local repos to install the local hooks into
+
+
 
 local_repos="$HOME/test"
 # Install hooks
