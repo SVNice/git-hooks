bc34aa29e44ad47f12b28cc41b8de0f62b134e96
SVNice <22481082+SVNice@users.noreply.github.com>
2022-02-24 17:59:37 +0000

    Install custom hooks

--- install.sh
+++ install.sh
@@ -1,39 +1,73 @@
 #!/usr/bin/env bash
-# 
-#
+# Install script to install git hooks into git repos and install needed programs
+# for said scripts.
+install_default_git=true
 search_for_local_repos=true
-install_tox=true
-install_hooks=["all"] # Just passing all will install all hooks in ./hooks/
+hooks=(hooks/python/*)
+
+git_template_location=/usr/share/git-core/templates
+
+local_repos=()
 # Get all arguments and process them
 args=("$@")
 index=0
 # If argument is not empty
 while [[ -n "${args[$index]}" ]]; do
-   current_arg="${args[$index]}"
-   case "$current_arg" in
-           "--test" | "-t")
-                   index=$index+1
-                   echo "test arg is ${args[$index]}"
-                   ;;
-           *)
-                   echo "Unkown argument $current_arg"
-                   exit 1
-   esac
-   index=$index+1
+    current_arg="${args[$index]}"
+    case "$current_arg" in
+        "--help")
+echo "Install script for git hooks. Automatically installs into
+all repos found under the HOME directory.
+FLAGS:
+    --dir -d    Select sepcfic repo to install to. Must be the
+    the root of the repo
+    --help      View this help test
+    --hook -h   To Install a specific hook. Can use globing."
+            exit 0
+            ;;
+        "--dir" | "-d")
+            index=$index+1
+            local_repos+=("${args[$index]}")
+            search_for_local_repos=false
+            ;;
+        "--no-default" | "-n")
+            install_default_git=false
+            ;;
+        "--hook" | "-h")
+            index=$index+1
+            hooks=("${args[$index]}")
+            ;;
+        *)
+            echo "Unkown argument $current_arg"
+            exit 1
+    esac
+    index=$index+1
 done
 
-
 # Get all local repos to install the local hooks into
+if [[ $search_for_local_repos == "true" ]]; then 
+    local_repos=$(find $HOME -name ".git")
+fi
 
-
-
-local_repos="$HOME/test"
 # Install hooks
-for repo in local_repos; do
-    echo "Installing hooks into repo: "+repo
-    cp -r hooks $repo/.git/hooks
-    # TODO make this more intelgent and only make execuatable our files
-    chmod +x $repo/.git/hooks/*
+for repo in $local_repos; do
+    echo "Installing hooks into repo: $repo"
+    for hook in $hooks; do
+        echo "loop"
+        hook=echo $hook | sed -rn 's/.*hooks\/(.*)/\1/p'
+        cp "$hook" "$repo/git/hooks/"
+        chmod +x "$repo/git/$hook"
+    done
+done
+
+# Install to default repo
+if [[ $install_default_git == "false" ]]; then
+    for hook in $hooks; do
+        echo "loop"
+        hook=echo $hook | sed -rn 's/.*hooks\/(.*)/\1/p'
+        cp "$hook" "$repo/git/hooks/"
+        chmod +x "$repo/git/$hook"
+    done
 done
 
 echo "Done"
